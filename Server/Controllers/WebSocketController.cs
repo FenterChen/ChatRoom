e97e16dda4c06496b6e5d6124852605bf1ff1464
Daniel <dj0806448@gmail.com>
2024-01-23 18:59:44 +0800

    1.adjust git 2.Start writing the code for WebSocketController and User

---
+++ Server/Controllers/WebSocketController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace Server.Controllers
+{
+    public class WebSocketController : ControllerBase
+    {
+        private readonly Serilog.ILogger _serilog = Log.ForContext<WebSocketController>();
+        [HttpGet("/ws")]
+        public async Task Get()
+        {
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                _serilog.Warning(ex, $"WebSocketController Exception:");
+            }
+        }
+    }
+}
---
+++ Server/Models/User.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Server.Models
+{
+    public enum Role
+    {
+        User = 0,
+        Machine = 1,
+        MainServer = 2,
+        BackupServer = 3,
+    };
+    public class User
+    {
+        public string? Id { get; private set; }
+
+        public Role Role { get; private set; }
+        public string? AvailableRoomId { get; private set; }
+        public BaseRoom? Room; // 所在房間
+        private WebsocketManager? websocketManager;
+
+        public bool IsOnline { get => websocketManager?.IsOnline ?? false; }
+
+        public void SetUserId(string id)
+        {
+            Id ??= id;
+        }
+
+        public void SetRole(Role role)
+        {
+            Role = role;
+        }
+
+        public void SetAvailableRoomId(string? RoomId)
+        {
+            AvailableRoomId = RoomId;
+        }
+
+        public static string SerialNumberWithTimeAndGuid()
+        {
+            string timestamp = DateTime.UtcNow.ToString("yyyyMMddHHmmssfff");
+            string guidPart = Guid.NewGuid().ToString("N").Substring(0, 8).ToUpper();
+            return timestamp + "-" + guidPart;
+        }
+
+        public void SetWebsocketManager(WebsocketManager websocketManager)
+        {
+            this.websocketManager = websocketManager;
+        }
+
+        public void Send(string type, int error, object data)
+        {
+            websocketManager?.Send(type, error, data);
+        }
+
+        public void Send(object DataDto)
+        {
+            websocketManager?.Send(DataDto);
+        }
+
+        public async Task Close()
+        {
+            if (websocketManager != null)
+            {
+                await websocketManager.Close();
+            }
+        }
+    }
+}
