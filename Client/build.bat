86a44062cb631d4c59111be1e4948a64315e07ad
FenterChen <di0806448@gmail.com>
2024-01-26 23:04:08 +0800

    v1.0.0

--- Client/build.bat
+++
@@ -1,5 +0,0 @@
-set version=v0.1.4
-docker build -t 34.81.83.64:3100/h5-game/pusher-client:%version% .
-docker push 34.81.83.64:3100/h5-game/pusher-client:%version%
-@REM GCP
-@REM docker run -it -d --name pusher-client -p 2005:80 34.81.83.64:3100/h5-game/pusher-client:%version%
--- Server/build.bat
+++
@@ -1,8 +0,0 @@
-set version=v0.0.4
-
-docker build -t fenterchen/chatserver:%version% .
-
-docker push fenterchen/chatserver:%version%
-
-@REM GCP
-@REM sudo docker run -it -d --name chatserver -p 443:443 --link iceserver:iceserver fenterchen/chatserver:v0.0.3
