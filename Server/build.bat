cb330951424085a4feff1133b9ed585ef4ed8e46
FenterChen <di0806448@gmail.com>
2024-03-17 22:38:42 +0800

    v0.0.6

--- Server/build.bat
+++ Server/build.bat
@@ -1,8 +1,8 @@
-set version=v0.0.5-dev
+set version=v0.0.6
 
 docker build -t fenterchen/chatserver:%version% .
 
 docker push fenterchen/chatserver:%version%
 
 @REM GCP
-@REM sudo docker run -it -d --name chatserver -p 443:443 --link iceserver:iceserver fenterchen/chatserver:v0.0.3
+@REM sudo docker run -it -d --name chatserver -p 443:443 --link iceserver:iceserver fenterchen/chatserver:v0.0.6
